a392ffa9c698cabc2445a107dd994bfcc21de0bd
2EZ4BLINK <blinkcode24@gmail.com>
2026-06-27 23:41:10 +0800

    inital commit

---
+++ MasteringCSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿int firstNumber;
+int secondNumber;
+
+Console.WriteLine("Type first number: ");
+string firstInput = Console.ReadLine();
+firstNumber = int.Parse(firstInput);
+
+
+Console.WriteLine("Type second number: ");
+string secondInput = Console.ReadLine();
+secondNumber = int.Parse(secondInput);
+
+Console.WriteLine($"Your sum: {firstNumber + secondNumber}");
