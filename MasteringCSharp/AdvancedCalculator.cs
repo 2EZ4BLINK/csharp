0949890a717b42cc22ed47b052318966c6d828a8
2EZ4BLINK <blinkcode24@gmail.com>
2026-06-28 08:51:02 +0800

    update: a little advance calculator

---
+++ MasteringCSharp/AdvancedCalculator.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Enter your first number: ");
+double firstNumber = double.Parse(Console.ReadLine());
+Console.Write("Enter your Second number: ");
+double secondNumber = double.Parse(Console.ReadLine());
+
+Console.Write("Choos an operation (-,+,*,/): ");
+char chosenOperator = Console.ReadLine()[0];
+
+if(chosenOperator == '+') {
+    Console.Write($"Your result: {firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
+} else if (chosenOperator == '-') {
+    Console.Write($"Your result: {firstNumber} + {secondNumber} = {firstNumber - secondNumber}");
+} else if (chosenOperator == '*') {
+    Console.Write($"Your result: {firstNumber} + {secondNumber} = {firstNumber * secondNumber}");
+} else if (chosenOperator == '/') {
+    if(firstNumber == 0 || secondNumber == 0) {
+        Console.Write("Dont divide zero value in division");
+    } else {
+        Console.Write($"Your result: {firstNumber} + {secondNumber} = {firstNumber / secondNumber}");
+    }
+}
