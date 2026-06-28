03c02bc12f4170b5139ff613f2280c5f2715dee6
2EZ4BLINK <blinkcode24@gmail.com>
2026-06-28 14:02:10 +0800

    update: added a adventure game

--- MasteringCSharp/AdvancedCalculator.cs
+++ MasteringCSharp/AdvancedCalculator.cs
@@ -1,21 +1,46 @@
-﻿Console.Write("Enter your first number: ");
-double firstNumber = double.Parse(Console.ReadLine());
-Console.Write("Enter your Second number: ");
-double secondNumber = double.Parse(Console.ReadLine());
+﻿Console.WriteLine("Choose a program:");
+Console.WriteLine("1. Calculator");
+Console.WriteLine("2. Adventure Game");
 
-Console.Write("Choos an operation (-,+,*,/): ");
-char chosenOperator = Console.ReadLine()[0];
+string choice = Console.ReadLine();
 
-if(chosenOperator == '+') {
-    Console.Write($"Your result: {firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
-} else if (chosenOperator == '-') {
-    Console.Write($"Your result: {firstNumber} + {secondNumber} = {firstNumber - secondNumber}");
-} else if (chosenOperator == '*') {
-    Console.Write($"Your result: {firstNumber} + {secondNumber} = {firstNumber * secondNumber}");
-} else if (chosenOperator == '/') {
-    if(firstNumber == 0 || secondNumber == 0) {
-        Console.Write("Dont divide zero value in division");
-    } else {
-        Console.Write($"Your result: {firstNumber} + {secondNumber} = {firstNumber / secondNumber}");
+if (choice == "1")
+{
+    Console.Write("Enter your first number: ");
+    double firstNumber = double.Parse(Console.ReadLine());
+
+    Console.Write("Enter your second number: ");
+    double secondNumber = double.Parse(Console.ReadLine());
+
+    Console.Write("Choose an operation (-,+,*,/): ");
+    char chosenOperator = Console.ReadLine()[0];
+
+    if (chosenOperator == '+')
+    {
+        Console.WriteLine(firstNumber + secondNumber);
     }
+    else if (chosenOperator == '-')
+    {
+        Console.WriteLine(firstNumber - secondNumber);
+    }
+    else if (chosenOperator == '*')
+    {
+        Console.WriteLine(firstNumber * secondNumber);
+    }
+    else if (chosenOperator == '/')
+    {
+        if (secondNumber == 0)
+        {
+            Console.WriteLine("Cannot divide by zero.");
+        }
+        else
+        {
+            Console.WriteLine(firstNumber / secondNumber);
+        }
+    }
+}
+else if (choice == "2")
+{
+    AdventureGame adventure = new AdventureGame();
+    adventure.Start();
 }
